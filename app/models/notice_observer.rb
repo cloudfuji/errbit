8f3ab84cc3d1e8f627a7613ffc83b5395c2424de
Sean Grove <s@bushi.do>
2012-02-22 09:11:01 -0800

    Adds in the notice observer to publish bushido events

---
+++ app/models/notice_observer.rb
@@ -0,0 +1,26 @@
+class NoticeObserver < Mongoid::Observer
+  def after_create(notice)
+    if ::Bushido::Platform.on_bushido?
+      @notice = notice
+      @err    = notice.err
+      @app    = notice.problem.app
+
+      human_message = notice_title(notice.err.problem)
+      human_message += " see more at #{Rails.application.routes.url_helpers.app_err_notice_url(@app, @err, @notice, :host => ENV['BUSHIDO_DOMAIN'])}"
+      event = {
+        :category => :app,
+        :name     => :error,
+        :data     => {
+          :human  => human_message,
+          :source => "Errbit",
+          :url    => Rails.application.routes.url_helpers.app_err_notice_url(@app, @err, @notice, :host => ENV['BUSHIDO_DOMAIN'])
+        }
+      }
+      ::Bushido::Event.publish(event)
+    end
+  end
+
+  def notice_title(notice)
+    "[#{@app.name}][#{@notice.environment_name}] #{@notice.message}"
+  end
+end
