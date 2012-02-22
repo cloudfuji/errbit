bfaa5b4b473b18465f0f9136919b01d39e2e31da
Sean Grove <s@bushi.do>
2012-02-22 05:25:25 -0800

    Adds in Bushido hooks for apps and user events

---
+++ lib/bushido/bushido_hooks/app_hooks.rb
@@ -0,0 +1,13 @@
+class BushidoAppHooks < Bushido::EventObserver
+  def app_claimed
+    puts "Updating #{User.first.inspect} with incoming data #{params.inspect}"
+    puts "Devise username column: #{::Devise.cas_username_column}="
+    puts "Setting username to: #{params.try(:[], 'ido_id')}"
+
+    user = User.first
+    user.email = params['data'].try(:[], 'email')
+    user.name = user.email.split('@').first
+    user.send("#{::Devise.cas_username_column}=".to_sym, params['data'].try(:[], 'ido_id'))
+    user.save
+  end
+end
