1fca414953de89d15185846da01ba560f5fb99af
Sean Grove <s@bushi.do>
2012-02-28 16:24:24 -0800

    Updates the notice publisher for Bushido

--- app/models/notice_observer.rb
+++ app/models/notice_observer.rb
@@ -6,14 +6,19 @@ class NoticeObserver < Mongoid::Observer
       @app    = notice.problem.app
 
       human_message = notice_title(notice.err.problem)
-      human_message += " see more at #{Rails.application.routes.url_helpers.app_err_url(@app, @err, :host => ENV['BUSHIDO_DOMAIN'])}"
+      human_message += " see more at #{Rails.application.routes.url_helpers.app_err_url(@app, @notice.problem, :host => ENV['BUSHIDO_DOMAIN'])}"
       event = {
         :category => :app,
         :name     => :errored,
         :data     => {
           :human  => human_message,
-          :source => "Errbit",
-          :url    => Rails.application.routes.url_helpers.app_err_url(@app, @err, :host => ENV['BUSHIDO_DOMAIN'])
+          :environment_name => @notice.environment_name,
+          :occurrences      => @notice.problem.notices_count,
+          :message          => @notice.message,
+          :app_backtrace    => @notice.app_backtrace,
+          :request          => @notice.request,
+          :source           => "Errbit",
+          :url              => Rails.application.routes.url_helpers.app_err_url(@app, @err, :host => ENV['BUSHIDO_DOMAIN'])
         }
       }
       ::Bushido::Event.publish(event)
